7b4763fc21a23b60d9760d7421e05e9c09866a6a
Chetan Bhuyal <chetanbhuyal@gmail.com>
2026-03-12 16:41:11 +0530

    fix render port

--- app.py
+++ app.py
@@ -87,6 +87,9 @@ def metrics():
     return jsonify(get_metrics())
 
 
+import os
+
 if __name__ == '__main__':
     ensure_model()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
